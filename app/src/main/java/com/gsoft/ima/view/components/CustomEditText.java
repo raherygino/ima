b084f1be9c227ac5552a83395bf46b4d3155aa16
gino <georgino197@gmail.com>
2023-06-07 08:52:05 +0200

    Set font edit text

---
+++ app/src/main/java/com/gsoft/ima/view/components/CustomEditText.java
@@ -0,0 +1,29 @@
+package com.gsoft.ima.view.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.gsoft.ima.R;
+
+public class CustomEditText extends AppCompatEditText {
+    private String customFont;
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+    private void init(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomEditText);
+
+        customFont = typedArray.getString(R.styleable.CustomEditText_customFont);
+        if (customFont != null) {
+            Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), customFont);
+            setTypeface(typeface);
+        }
+
+        typedArray.recycle();
+    }
+}
