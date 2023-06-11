90e1329295b180fb61d4444dda94ad8a42beb42f
gino <georgino197@gmail.com>
2023-06-11 08:10:54 +0200

    Signup: Date picker

--- app/src/main/java/com/gsoft/ima/ui/signup/SignupActivity.java
+++ app/src/main/java/com/gsoft/ima/ui/signup/SignupActivity.java
@@ -1,15 +1,22 @@
 package com.gsoft.ima.ui.signup;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.gsoft.ima.databinding.ActivitySignupBinding;
 import com.gsoft.ima.ui.login.LoginActivity;
+import com.gsoft.ima.utils.eventlistener.DateSet;
+import java.util.Calendar;
 
 public class SignupActivity extends AppCompatActivity {
 
     ActivitySignupBinding binding;
+    SignupActivityViewModel viewModel;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,8 +24,17 @@ public class SignupActivity extends AppCompatActivity {
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SignupActivityViewModel viewModel = new SignupActivityViewModel(this);
+        viewModel = new SignupActivityViewModel(this);
         binding.setViewModel(viewModel);
+        binding.birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog dialog = new DatePickerDialog(SignupActivity.this, new DateSet(viewModel.birthday), calendar.get(Calendar.YEAR)-70, 0, 1);
+                dialog.setCancelable(true);
+                dialog.show();
+            }
+        });
     }
 
     @Override
