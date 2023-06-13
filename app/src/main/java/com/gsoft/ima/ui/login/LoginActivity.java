94a8990dd8fb45d75c7ba785283a39d05a4b4d62
raherygino <georgino197@gmail.com>
2023-06-13 12:02:21 +0300

    Get phone number

--- app/src/main/java/com/gsoft/ima/ui/login/LoginActivity.java
+++ app/src/main/java/com/gsoft/ima/ui/login/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.gsoft.ima.ui.login;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.gsoft.ima.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    private static final int REQUEST_READ_PHONE_STATE = 1;
+    LoginActivityViewModel viewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,10 +23,38 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        LoginActivityViewModel viewModel = new LoginActivityViewModel(this);
+        viewModel = new LoginActivityViewModel(this);
         binding.setViewModel(viewModel);
 
-        viewModel.phone.set("0346500700");
+        //viewModel.phone.set("0346500700");
         viewModel.password.set("1234567890");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+        } else {
+            getPhoneNumber();
+        }
     }
+    private void getPhoneNumber() {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        if (telephonyManager != null) {
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                String phoneNumber = telephonyManager.getLine1Number();
+                viewModel.phone.set(phoneNumber);
+                // Do something with the phone number
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_READ_PHONE_STATE && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getPhoneNumber();
+        }
+    }
+
 }
