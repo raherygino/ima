43a346c32260adb79f5ca32c519970b978d5f4b2
gino <georgino197@gmail.com>
2023-06-19 12:09:27 +0200

    Debug: send sms

--- app/src/main/java/com/gsoft/ima/ui/main/MainActivity.java
+++ app/src/main/java/com/gsoft/ima/ui/main/MainActivity.java
@@ -3,24 +3,72 @@ package com.gsoft.ima.ui.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.gsoft.ima.R;
 import com.gsoft.ima.databinding.ActivityMainBinding;
 import com.gsoft.ima.databinding.BottomSheetSendMoneyBinding;
+import com.gsoft.ima.utils.BluetoothHelper;
+import com.gsoft.ima.utils.BluetoothHelperReceiver;
 
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private BottomSheetBehavior<View> bottomSheetBehavior;
 
+    private BluetoothHelperReceiver bluetoothHelperReceiver;
+
+    private final Handler messageHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            if (message.what == 0) {
+                String receivedMessage = (String) message.obj;
+                Toast.makeText(getApplicationContext(), "Received message: " + receivedMessage, Toast.LENGTH_SHORT).show();
+                // Do something with the received message
+            }
+            return true;
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        String deviceAddress = "00:00:00:00:00:00"; // Replace with the Bluetooth device address
+        String message = "Hello, this is a test message.";
+
+        BluetoothHelper bluetoothHelper = new BluetoothHelper(this);
+
+        if (bluetoothHelper.isBluetoothSupported() && bluetoothHelper.isBluetoothEnabled()) {
+            if (bluetoothHelper.connectToDevice(deviceAddress)) {
+                bluetoothHelper.sendMessage(message);
+                bluetoothHelper.disconnect();
+            } else {
+                Toast.makeText(getApplicationContext(), "Failed to connect to device.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Bluetooth is not supported or not enabled.", Toast.LENGTH_SHORT).show();
+        }
+
+        bluetoothHelperReceiver = new BluetoothHelperReceiver(messageHandler);
+
+        if (bluetoothHelperReceiver.isBluetoothSupported() && bluetoothHelperReceiver.isBluetoothEnabled()) {
+            if (bluetoothHelperReceiver.startServer()) {
+                Toast.makeText(getApplicationContext(), "Server started. Waiting for incoming messages...", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Failed to start server.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Bluetooth is not supported or not enabled.", Toast.LENGTH_SHORT).show();
+        }
+
         bottomSheetBehavior = BottomSheetBehavior.from(binding.designBottomSheet);
         binding.sendMoney.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bluetoothHelperReceiver.stopServer();
     }
 }
--- app/src/main/java/com/gsoft/ima/utils/SMSHelper.java
+++ app/src/main/java/com/gsoft/ima/utils/SMSHelper.java
@@ -1,16 +1,21 @@
 package com.gsoft.ima.utils;
 
+import android.content.Context;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 
 public class SMSHelper {
-    public static void sendSMS(String phoneNumber, String message) {
+    private Context context;
+    public SMSHelper(Context context) {
+        this.context = context;
+    }
+    public void sendSMS(String phoneNumber, String message) {
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, message, null, null);
-            Toast.makeText(getApplicationContext(), "Message sent!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Message sent!", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "Failed to send message.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Failed to send message.", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
