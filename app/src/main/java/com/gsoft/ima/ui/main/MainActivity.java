adb53a4164d8614997c7b220afa626ca73e3f9fe
gino <georgino197@gmail.com>
2023-06-22 20:42:01 +0200

    Fragement home

--- app/src/main/java/com/gsoft/ima/ui/main/MainActivity.java
+++ app/src/main/java/com/gsoft/ima/ui/main/MainActivity.java
@@ -8,19 +8,34 @@ import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.gsoft.ima.R;
 import com.gsoft.ima.databinding.ActivityMainBinding;
+import com.gsoft.ima.ui.main.home.FragmentHome;
 import com.gsoft.ima.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private String currentFragment;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Utils.setColorBarStatus(this);
+        setFragment(new FragmentHome());
+    }
+
+    public void setFragment(Fragment fragment) {
+        currentFragment =  fragment.getClass().toString().replace("class", "");
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_main, fragment);
+        fragmentTransaction.commit();
     }
 }
