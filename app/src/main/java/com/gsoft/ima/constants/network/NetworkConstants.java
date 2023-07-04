3dad2731cd595e1339ddcf8d5a11fb6d05d3d06b
raherygino <georgino197@gmail.com>
2023-07-04 10:47:05 +0200

    Debug: forgot password

--- app/src/main/java/com/gsoft/ima/constants/network/NetworkConstants.java
+++ app/src/main/java/com/gsoft/ima/constants/network/NetworkConstants.java
@@ -3,5 +3,6 @@ package com.gsoft.ima.constants.network;
 public class NetworkConstants {
     ///http://192.168.43.162/ima-api/
     ///http://10.0.2.2/ima-api/
-    public static final String BASE_URL = "http://192.168.43.162/ima-api/";
+    ///https://ima.eluos.mg/
+    public static final String BASE_URL = "https://ima.eluos.mg/";
 }
